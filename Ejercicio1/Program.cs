44071deb05e6d4c83b09afac53585f916b31d2dd
Milena <ferreyramilenaarlene26@gmail.com>
2025-05-25 16:20:43 -0300

    Ejercicio1

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Ingrese un número a invertir");
+string respuesta = Console.ReadLine();
+
+int num;
+
+if (int.TryParse(respuesta, out num))
+{
+    if (num > 9)
+    {
+        Console.WriteLine("El número invertido es:");
+        while (num > 0)
+        {
+            int dig = num % 10;
+            Console.Write(dig);
+            num = num / 10;
+        }
+    }
+    else
+    {   if (num > 0 && num < 10)
+        {
+            Console.WriteLine("El número invertido es:");
+            Console.Write(num);
+        }
+        else
+        {
+            Console.WriteLine("El número tiene que ser mayor a 0.");
+        }
+    }
+}
+else
+{
+    Console.WriteLine("Número inválido.");
+}
+
